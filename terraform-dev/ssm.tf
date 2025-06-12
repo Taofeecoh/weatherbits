654adc2742e0559c7d59b0348ea359215fd06d04
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-06-12 18:26:58 +0100

    updated ssm parameters

--- terraform-dev/ssm.tf
+++ terraform-dev/ssm.tf
@@ -1,12 +1,12 @@
 resource "aws_ssm_parameter" "airflow_tao_KeyID" {
   name  = "airflow_tao_KeyID"
   type  = "String"
-  value = var.airflow_tao_KeyID
+  value = aws_iam_access_key.airflow_tao.id
 }
 
 
 resource "aws_ssm_parameter" "airflow_tao_secretKey" {
   name  = "airflow_tao_secretKey"
   type  = "SecureString"
-  value = var.airflow_tao_secretKey
+  value = aws_iam_access_key.airflow_tao.secret
 }
