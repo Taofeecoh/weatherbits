360a2a206fabf74d3770ea3e31d23e1a6be7975e
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-07-05 23:46:18 +0100

    iam

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -42,3 +42,17 @@ resource "aws_iam_user_policy_attachment" "airflow" {
 resource "aws_iam_access_key" "airflow_tao" {
   user = aws_iam_user.airflow_tao.name
 }
+
+
+resource "aws_ssm_parameter" "airflow_tao_KeyID" {
+  name  = "airflow_tao_KeyID"
+  type  = "String"
+  value = aws_iam_access_key.airflow_tao.id
+}
+
+
+resource "aws_ssm_parameter" "airflow_tao_secretKey" {
+  name  = "airflow_tao_secretKey"
+  type  = "SecureString"
+  value = aws_iam_access_key.airflow_tao.secret
+}
