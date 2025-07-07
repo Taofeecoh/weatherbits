4d23af957424a8d7b232b2c79fffe8f6333ffc6c
Taofeecoh <adesanutaofeecoh@gmail.com>
2025-07-07 02:02:11 +0100

    dag

--- orchestration/dags/dag_tasks/agro_pipeline_dag.py
+++ orchestration/dags/dag_tasks/agro_pipeline_dag.py
@@ -6,16 +6,16 @@ from airflow.operators.python import PythonOperator
 from dag_tasks import airflow_variable
 from utils.app import api_to_storage
 
-
 airflow_vars = airflow_variable
 
 my_path = "s3://tao-general-ingestion2/airflow-weatherbits-dump/"
-file_path = "{}weatherbits-{}.parquet".format(my_path, time.strftime("%Y-%m-%d|%H:%M:%S"))
+file_path = "{}weatherbits-{}.parquet".format(
+    my_path, time.strftime("%Y-%m-%d|%H:%M:%S"))
 
-base_url = 'https://api.weatherbit.io/v2.0/forecast/agweather' 
+base_url = 'https://api.weatherbit.io/v2.0/forecast/agweather'
 params = {
     'lat': '9.896527',  # Jos N source: maps of world
-    'lon': '8.858331', # Jos E
+    'lon': '8.858331',  # Jos E
     'key': Variable.get("WEATHERBITS_API_SECRET_KEY")
         }
 
